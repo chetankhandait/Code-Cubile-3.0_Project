592cacbc7b4e2a1981012cecc4c7068ab001c582
ARYAN TYAGI <aryantyagi111s@gmail.com>
2024-09-15 16:54:42 +0530

    fixed ui

--- Client/my-project/src/App.jsx
+++ Client/my-project/src/App.jsx
@@ -10,22 +10,16 @@ import FinanceAssistance from "./pages/FinanceAssistance";
 function App() {
   return (
     <div>
-      
       {/* <Navbar /> */}
-      <div className='pt-16'>
-
-      <Routes>
-        <Route path="/" element={<Homepage />} />
-        <Route path="/multi-form" element={<MultiStepForm />} />
-        <Route path="/portfolio" element={<Portfolio />} />
-        <Route path="/finance-assistant" element={<FinanceAssistance />} />
-        <Route path="/multistep-processing" element={<MultiStepForm />} />
-      </Routes>
-
+      <div>
+        <Routes>
+          <Route path="/" element={<Homepage />} />
+          <Route path="/multi-form" element={<MultiStepForm />} />
+          <Route path="/portfolio" element={<Portfolio />} />
+          <Route path="/finance-assistant" element={<FinanceAssistance />} />
+          <Route path="/multistep-processing" element={<MultiStepForm />} />
+        </Routes>
       </div>
-    
- 
-      
 
       {/* <FinancialAdvisor/> */}
       {/* <Portfolio/> */}
